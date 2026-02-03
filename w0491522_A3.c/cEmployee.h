fb6fb01d65ba6f57329f0d4d2bfa29819917f25f
Oto-Toya <bennytanwt@outlook.com>
2026-02-03 17:04:44 -0400

    Refactor ownercopy method signature in cEmployee class

--- w0491522_A3.c/cEmployee.h
+++ w0491522_A3.c/cEmployee.h
@@ -1,20 +1,36 @@
 #pragma once
 #include <iostream>
+#include <cstring>
 
-class cEmployee; // Forward declaration
 using namespace std;
 
+// cEmployee class definition - represents an employee who owns a car
 class cEmployee
 {
 public:
+	// Constructor - initializes employee object and allocates dynamic memory
 	cEmployee();
+	
+	// Destructor - frees dynamically allocated memory
 	~cEmployee();
+	
+	// Gets employee data from user input
 	void getEmployeeData();
+	
+	// Displays all employee data
 	void showEmployeeData();
+	
+	// Allows user to modify employee data
 	void mutateEmployeeData();
-	void ownercopy(const cEmployee& dest,const cEmployee& src);
+	
+	// Copies employee data from source to destination
+	void ownercopy(cEmployee& dest, const cEmployee& src);
+	
 private:
+	// Dynamically allocated string for employee name (20 chars max)
 	char* _name;
+	
+	// Dynamically allocated string for employee department (20 chars max)
 	char* _department;
 };
 
